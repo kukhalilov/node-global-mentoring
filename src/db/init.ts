b19c909517e56354a80a23abfc368138827f82fa
Kuvonchbek Khalilov <kukhalilov@gmail.com>
2023-01-03 09:22:58 +0500

    feat: add predefined users to db

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,7 +1,39 @@
 import User from './models/User.js';
+
 const isDev = process.env.NODE_ENV === 'development';
 
-const dbInit = () => {
-    User.sync({ alter: isDev });
+const dbInit = async () => {
+    // drop users table and create new one
+    await User.drop();
+    await User.sync({ alter: isDev });
+
+    // add some predefined users to db
+    await User.bulkCreate([
+        {
+            login: 'admin',
+            password: 'pass1',
+            age: 20,
+            isDeleted: false
+        },
+        {
+            login: 'user1',
+            password: 'pass2',
+            age: 32,
+            isDeleted: false
+        },
+        {
+            login: 'user2',
+            password: 'pass3',
+            age: 25,
+            isDeleted: false
+        },
+        {
+            login: 'user3',
+            password: 'pass4',
+            age: 20,
+            isDeleted: false
+        }
+    ]) as User[];
 };
+
 export default dbInit;
