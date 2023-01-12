7d89e2d354ce3608928bac4909d5aa4e8aca3785
Kuvonchbek Khalilov <kukhalilov@gmail.com>
2023-01-12 08:55:19 +0500

    fix: fix status codes and error messages

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -12,8 +12,8 @@ usersRouter.get('/', async (req: Request, res: Response) => {
             : '';
         const result = await userController.getAutoSuggestUsers(loginSubstring, limit);
         return res.status(200).send(result);
-    } catch (error) {
-        return res.send(error);
+    } catch (error: any) {
+        return res.status(404).send(error.message);
     }
 });
 
@@ -22,8 +22,8 @@ usersRouter.get('/:id', async (req: Request, res: Response) => {
         const id = req.params.id;
         const result = await userController.getById(id);
         return res.status(200).send(result);
-    } catch (error) {
-        return res.send(error);
+    } catch (error: any) {
+        return res.status(404).send(error.message);
     }
 });
 
@@ -31,9 +31,9 @@ usersRouter.post('/', async (req: Request, res: Response) => {
     try {
         const payload:CreateUserDTO = req.body;
         const result = await userController.create(payload);
-        return res.status(200).send(result);
-    } catch (error) {
-        return res.send(error);
+        return res.status(201).send(result);
+    } catch (error: any) {
+        return res.status(404).send(error.message);
     }
 });
 
@@ -42,9 +42,9 @@ usersRouter.put('/:id', async (req: Request, res: Response) => {
         const id = req.params.id;
         const payload:UpdateUserDTO = req.body;
         const result = await userController.update(id, payload);
-        return res.status(201).send(result);
-    } catch (error) {
-        res.send(error);
+        return res.status(200).send(result);
+    } catch (error: any) {
+        res.status(404).send(error.message);
     }
 });
 
@@ -52,9 +52,9 @@ usersRouter.delete('/:id', async (req: Request, res: Response) => {
     try {
         const id = req.params.id;
         await userController.deleteById(id);
-        return res.status(204).send('User successfully deleted');
-    } catch (error) {
-        return res.send(error);
+        return res.status(200).send('User successfully deleted');
+    } catch (error: any) {
+        return res.status(404).send(error.message);
     }
 });
 
